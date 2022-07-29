ff9b2c27dbb4f2b6c12c376cd49156d9b87fd57c
Jose <86510147+jhlimardo@users.noreply.github.com>
2022-07-29 11:58:25 -0500

    endpoint route

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.REACT_APP_API
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

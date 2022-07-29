eae2d451edc48934c66441b186c12d50282d3c3a
Jose <86510147+jhlimardo@users.noreply.github.com>
2022-07-29 18:27:51 -0500

    dotenv

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 //const port = 3001; // defino un puerto donde va a eschuchar el servidor
 
 app.listen(process.env.PORT, () => {
-  console.log(`%s listening at port: ${process.env.PORT}`);
+  console.log(`%s listening at port: ${process.env.PORT}`, "Server");
 });
